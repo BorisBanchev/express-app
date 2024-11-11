8e5e4dc46eec7444a727c524bc45b517911d758d
BorisBanchev <boris.banchev@helsinki.com>
2024-11-11 14:59:09 +0200

    made change

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello my friend!</h1>");
+  res.send("<h1>Hello my friend! I made some changes</h1>");
 });
 
 const PORT = 8080;
